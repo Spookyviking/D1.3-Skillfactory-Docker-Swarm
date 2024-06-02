10263c93f208aa9828691d46ef422a7970547332
Maksim Ostapenko <spookyvikingooth@gmail.com>
2024-06-02 15:52:48 +0300

    ok

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "external_ip_address_manager" {
-  value = module.swarm_cluster[*].external_ip_address_manager
-}
-
-output "external_ip_address_worker" {
-  value = module.swarm_cluster[*].external_ip_address_worker
-}
